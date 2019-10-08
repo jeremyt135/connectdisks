dc32f8fdc71001bed575b752ba3b7e34366ca0fa
Jeremy Taylor <jdt13579@gmail.com>
2019-10-08 14:55:03 -0700

    Rename print method and add print macro

--- include/logging.hpp
+++ include/logging.hpp
@@ -6,13 +6,13 @@
 namespace util
 {
 	template<typename Arg0>
-	void print(std::ostream& out, std::string sep, Arg0&& arg0)
+	void printTo(std::ostream& out, std::string sep, Arg0&& arg0)
 	{
 		out << arg0;
 	}
 
 	template<typename Arg0, typename... Args>
-	void print(std::ostream& out, std::string sep, Arg0&& arg0, Args&&... args)
+	void printTo(std::ostream& out, std::string sep, Arg0&& arg0, Args&&... args)
 	{
 		out << arg0;
 		int dummy[sizeof...(Args)] = {(out << sep << args, 0)...};
@@ -21,7 +21,9 @@ namespace util
 
 
 #if defined DEBUG || defined _DEBUG
-#define printDebug(...) util::print(std::cout, " ", "[DEBUG]", __VA_ARGS__);
+#define printDebug(...) util::printTo(std::cout, " ", "[DEBUG]", __VA_ARGS__);
 #else
 #define printDebug(...) ((void)__VA_ARGS__)
 #endif
+
+#define print(...) util::printTo(std::cout, " ", __VA_ARGS__);
