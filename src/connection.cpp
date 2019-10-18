b6ab6a06c4d10dffe51fe322c6aab41cab7f7653
Jeremy Taylor <jdt13579@gmail.com>
2019-10-18 10:57:04 -0700

    Reorganize definitions in source file

--- src/connection.cpp
+++ src/connection.cpp
@@ -21,43 +21,26 @@ namespace game
 	{
 		namespace server
 		{
+			Connection::Connection(boost::asio::io_service & ioService, GameLobby* lobby) :
+				lobby{lobby},
+				socket{ioService},
+				pingTimer{ioService},
+				id{0},
+				clientIsConnected{true},
+				receivedPong{false},
+				missedPongs{0}
+			{
+			}
 
 			std::shared_ptr<Connection> Connection::create(boost::asio::io_service & ioService, GameLobby* lobby)
 			{
 				return std::shared_ptr<Connection>{new Connection{ioService, lobby}};
 			}
 
-			void Connection::onGameStart()
+			void Connection::onAccept()
 			{
-				// send id to client
-				std::shared_ptr<Message> message{new Message{}};
-				message->type = MessageType::gameStart;
-
-				// send the number of players
-				message->data[0] = lobby->getNumPlayers();
-
-				// send the board dimensions
-				auto* game = lobby->getGame();
-
-				message->data[1] = game->getNumColumns();
-				message->data[2] = game->getNumRows();
-				message->data[3] = game->getCurrentPlayer();
-
-				sendMessage(message);
-			}
-
-			void Connection::onGameEnd(uint8_t player)
-			{
-				sendWinner(player);
-			}
-
-			void Connection::sendWinner(uint8_t winner)
-			{
-				// tell client game has ended and which player won
-				std::shared_ptr<Message> message{new Message{}};
-				message->type = MessageType::gameEnd;
-				message->data[0] = winner;
-				sendMessage(message);
+				waitForMessages();
+				startPings();
 			}
 
 			void Connection::waitForMessages()
@@ -74,132 +57,6 @@ namespace game
 					boost::asio::buffer(message.get(), sizeof(Message)),
 					std::bind(
 						&Connection::onReadSocket,
-						shared_from_this(),
-						message,
-						std::placeholders::_1,
-						std::placeholders::_2
-					));
-			}
-
-			void Connection::setId(uint8_t id)
-			{
-				if (this->id == 0)
-				{
-					this->id = id;
-
-					// send id to client
-					std::shared_ptr<Message> message{new Message{}};
-					message->type = MessageType::connected;
-					message->data[0] = id;
-					sendMessage(message);
-				}
-			}
-
-			void Connection::onAccept()
-			{
-				waitForMessages();
-				startPings();
-			}
-
-			bool Connection::isAlive() const noexcept
-			{
-				return clientIsConnected;
-			}
-
-			uint8_t Connection::getId() const noexcept
-			{
-				return id;
-			}
-
-			void Connection::setGameLobby(GameLobby * lobby)
-			{
-				using std::placeholders::_1;
-				using std::placeholders::_2;
-				using std::placeholders::_3;
-				using std::bind;
-
-				this->lobby = lobby;
-				lobby->addTurnHandler(
-					GameLobby::TurnHandler(bind(&Connection::onTurn, shared_from_this(), _1)).track_foreign(shared_from_this()));
-				lobby->addTurnResultHandler(
-					GameLobby::TurnResultHandler(bind(&Connection::onUpdate, shared_from_this(), _1, _2, _3)).track_foreign(shared_from_this()));
-				lobby->addGameStartHandler(
-					GameLobby::GameStartHandler(bind(&Connection::onGameStart, shared_from_this())).track_foreign(shared_from_this()));
-				lobby->addGameEndHandler(
-					GameLobby::GameEndHandler(bind(&Connection::onGameEnd, shared_from_this(), _1)).track_foreign(shared_from_this()));
-			}
-
-			void Connection::notifyQueuePosition(uint64_t position)
-			{
-				std::shared_ptr<Message> message{new Message{}};
-				boost::endian::native_to_big_inplace(position);
-				message->type = MessageType::inQueue;
-				memcpy(&message->data[0], &position, sizeof(uint64_t));
-				sendMessage(message);
-			}
-
-			boost::asio::ip::tcp::socket& Connection::getSocket()
-			{
-				return socket;
-			}
-
-			Connection::Connection(boost::asio::io_service & ioService, GameLobby* lobby) :
-				lobby{lobby},
-				socket{ioService},
-				pingTimer{ioService},
-				id{0},
-				clientIsConnected{true},
-				receivedPong{false},
-				missedPongs{0}
-			{
-			}
-
-			void Connection::onTurn(uint8_t playerId)
-			{
-				if (playerId == id)
-				{
-					// tell client it's their turn
-					std::shared_ptr<Message> message{new Message{}};
-					message->type = MessageType::takeTurn;
-					message->data[0] = playerId;
-					message->data[1] = static_cast<uint8_t>(-1); // requesting turn
-					sendMessage(message);
-				}
-			}
-
-
-			void Connection::onUpdate(uint8_t playerId, uint8_t col, FourAcross::TurnResult result)
-			{
-
-				if (playerId != id)
-				{
-					if (result == FourAcross::TurnResult::success)
-					{
-						// tell client that a successful turn was taken by another player
-						std::shared_ptr<Message> message{new Message{}};
-						message->type = MessageType::update;
-						message->data[0] = playerId;
-						message->data[1] = col;
-						sendMessage(message);
-					}
-				}
-				else
-				{
-					handleTurnResult(result, col);
-				}
-			}
-
-			void Connection::sendMessage(std::shared_ptr<Message> message)
-			{
-				if (!clientIsConnected)
-				{
-					return;
-				}
-
-				boost::asio::async_write(socket,
-					boost::asio::buffer(message.get(), sizeof(Message)),
-					std::bind(
-						&Connection::onWriteSocket,
 						shared_from_this(),
 						message,
 						std::placeholders::_1,
@@ -282,6 +139,33 @@ namespace game
 				}
 			}
 
+			void Connection::sendMessage(std::shared_ptr<Message> message)
+			{
+				if (!clientIsConnected)
+				{
+					return;
+				}
+
+				boost::asio::async_write(socket,
+					boost::asio::buffer(message.get(), sizeof(Message)),
+					std::bind(
+						&Connection::onWriteSocket,
+						shared_from_this(),
+						message,
+						std::placeholders::_1,
+						std::placeholders::_2
+					));
+			}
+
+			void Connection::notifyQueuePosition(uint64_t position)
+			{
+				std::shared_ptr<Message> message{new Message{}};
+				boost::endian::native_to_big_inplace(position);
+				message->type = MessageType::inQueue;
+				memcpy(&message->data[0], &position, sizeof(uint64_t));
+				sendMessage(message);
+			}
+			
 			void Connection::handleDisconnect()
 			{
 				if (clientIsConnected)
@@ -306,6 +190,107 @@ namespace game
 				message->data[0] = toUnderlyingType(result);
 				message->data[1] = column;
 				sendMessage(message);
+			}
+
+			void Connection::setId(uint8_t id)
+			{
+				if (this->id == 0)
+				{
+					this->id = id;
+
+					// send id to client
+					std::shared_ptr<Message> message{new Message{}};
+					message->type = MessageType::connected;
+					message->data[0] = id;
+					sendMessage(message);
+				}
+			}
+
+			void Connection::setGameLobby(GameLobby * lobby)
+			{
+				this->lobby = lobby;
+				{
+					using std::placeholders::_1;
+					using std::placeholders::_2;
+					using std::placeholders::_3;
+					using std::bind;
+
+					lobby->addTurnHandler(
+						GameLobby::TurnHandler(bind(&Connection::onTurn, shared_from_this(), _1)).track_foreign(shared_from_this()));
+					lobby->addTurnResultHandler(
+						GameLobby::TurnResultHandler(bind(&Connection::onUpdate, shared_from_this(), _1, _2, _3)).track_foreign(shared_from_this()));
+					lobby->addGameStartHandler(
+						GameLobby::GameStartHandler(bind(&Connection::onGameStart, shared_from_this())).track_foreign(shared_from_this()));
+					lobby->addGameEndHandler(
+						GameLobby::GameEndHandler(bind(&Connection::onGameEnd, shared_from_this(), _1)).track_foreign(shared_from_this()));
+				}
+			}
+			
+			void Connection::onGameStart()
+			{
+				// send id to client
+				std::shared_ptr<Message> message{new Message{}};
+				message->type = MessageType::gameStart;
+
+				// send the number of players
+				message->data[0] = lobby->getNumPlayers();
+
+				// send the board dimensions
+				auto* game = lobby->getGame();
+
+				message->data[1] = game->getNumColumns();
+				message->data[2] = game->getNumRows();
+				message->data[3] = game->getCurrentPlayer();
+
+				sendMessage(message);
+			}
+
+			void Connection::onGameEnd(uint8_t player)
+			{
+				sendWinner(player);
+			}
+
+			void Connection::sendWinner(uint8_t winner)
+			{
+				// tell client game has ended and which player won
+				std::shared_ptr<Message> message{new Message{}};
+				message->type = MessageType::gameEnd;
+				message->data[0] = winner;
+				sendMessage(message);
+			}
+
+			void Connection::onTurn(uint8_t playerId)
+			{
+				if (playerId == id)
+				{
+					// tell client it's their turn
+					std::shared_ptr<Message> message{new Message{}};
+					message->type = MessageType::takeTurn;
+					message->data[0] = playerId;
+					message->data[1] = static_cast<uint8_t>(-1); // requesting turn
+					sendMessage(message);
+				}
+			}
+
+			void Connection::onUpdate(uint8_t playerId, uint8_t col, FourAcross::TurnResult result)
+			{
+
+				if (playerId != id)
+				{
+					if (result == FourAcross::TurnResult::success)
+					{
+						// tell client that a successful turn was taken by another player
+						std::shared_ptr<Message> message{new Message{}};
+						message->type = MessageType::update;
+						message->data[0] = playerId;
+						message->data[1] = col;
+						sendMessage(message);
+					}
+				}
+				else
+				{
+					handleTurnResult(result, col);
+				}
 			}
 
 			void Connection::pingOnTimer(const boost::system::error_code & error, boost::asio::steady_timer * timer)
@@ -345,6 +330,7 @@ namespace game
 					}
 				}
 			}
+
 			void Connection::sendPing()
 			{
 				printDebug("PING\n");
@@ -352,11 +338,27 @@ namespace game
 				message->type = MessageType::ping;
 				sendMessage(message);
 			}
+
 			void Connection::startPings()
 			{
 				sendPing();
 				pingTimer.expires_from_now(boost::asio::chrono::seconds(10));
 				pingTimer.async_wait(std::bind(&Connection::pingOnTimer, shared_from_this(), std::placeholders::_1, &pingTimer));
+			}
+
+			boost::asio::ip::tcp::socket& Connection::getSocket()
+			{
+				return socket;
+			}
+
+			bool Connection::isAlive() const noexcept
+			{
+				return clientIsConnected;
+			}
+
+			uint8_t Connection::getId() const noexcept
+			{
+				return id;
 			}
 		}
 	}
