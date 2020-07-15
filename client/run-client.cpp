241a96a8a1e6339348fb9c51384d485ec8ef1380
jeremyt135 <jdt13579@gmail.com>
2020-07-15 15:14:42 -0700

    Accept connection parameters from command line

--- client/run-client.cpp
+++ client/run-client.cpp
@@ -2,21 +2,63 @@
 #include "four-across/networking/client/consoleclient.hpp"
 
 #include <boost/asio.hpp>
+#include <boost/lexical_cast.hpp>
 
 #include <iostream>
+#include <string>
 #include <thread>
 
 using namespace game::networking::client;
 
-int main(int argc, char* argv[])
+struct Options
 {
+	std::string host;
+	uint16_t port;
+};
+
+void runClient(const Options& options){
 	try
 	{
 		std::unique_ptr<Client> gameClient{new ConsoleClient{}};
-		gameClient->connect("127.0.0.1", 8888);
+		gameClient->connect(options.host, options.port);
 	}
-	catch (std::exception& e)
+	catch (std::exception &e)
 	{
 		std::cerr << "An error occurred while running the client: " << e.what() << "\n";
 	}
+}
+const char* const usage = "Usage: client HOST PORT";
+void printUsage() {
+	std::cout << usage << std::endl;
+}
+
+Options getOptions(int argc, char *argv[])
+{
+	if (argc != 3) {
+		printUsage();
+		exit(EXIT_FAILURE);
+	}
+
+	Options options{};
+	try{
+		// ensure host is valid address
+		boost::system::error_code error;
+		boost::asio::ip::address_v4::from_string(argv[1], error);
+		if (error){
+			throw std::exception();
+		}
+		options.host = argv[1];
+		options.port = boost::lexical_cast<uint16_t>(argv[2]);
+	}catch(std::exception&){
+		printUsage();
+		exit(EXIT_FAILURE);
+	}
+
+	return options;
+}
+
+
+int main(int argc, char *argv[])
+{
+	runClient(getOptions(argc, argv));
 }
--- server/run-server.cpp
+++ server/run-server.cpp
@@ -1,35 +1,58 @@
 #include "four-across/networking/server/server.hpp"
 
 #include <boost/asio.hpp>
+#include <boost/lexical_cast.hpp>
 
 #include <iostream>
 
 using game::networking::server::Server;
 
-void runServer()
+struct Options
+{
+	uint16_t port;
+};
+
+void runServer(const Options &options)
 {
 	try
 	{
 		boost::asio::io_service service;
-		Server server{service, "127.0.0.1", 8888};
+		Server server{service, "0.0.0.0", options.port};
 		service.run();
 	}
-	catch (std::exception& e)
+	catch (std::exception &e)
 	{
-		std::cerr << e.what() << "\n";
+		std::cerr << "An error occurred while running the server: " << e.what() << "\n";
 	}
 }
 
-int main(int argc, char* argv[])
+const char* const usage = "Usage: server PORT";
+void printUsage() {
+	std::cout << usage << std::endl;
+}
+
+Options getOptions(int argc, char *argv[])
 {
-	try
-	{
-		runServer();
+	if (argc != 2) {
+		printUsage();
+		exit(EXIT_FAILURE);
 	}
-	catch (std::exception& e)
-	{
-		std::cerr << e.what() << "\n";
+
+	Options options{8888};
+	try{
+		options.port = boost::lexical_cast<uint16_t>(argv[1]);
+	}catch(boost::bad_lexical_cast&){
+		printUsage();
+		exit(EXIT_FAILURE);
 	}
+
+	return options;
+}
+
+int main(int argc, char *argv[])
+{
+	runServer(getOptions(argc, argv));
+
 	std::cout << "Press enter to exit..." << std::endl;
 	std::cin.clear();
 	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
